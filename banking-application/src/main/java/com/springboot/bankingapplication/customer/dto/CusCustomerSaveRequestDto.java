ff2df1bae26515a93c9021fa0b9ea0a7acf6fb23
sinandoseyici <sinandoseyici871@gmail.com>
2022-08-30 19:55:04 +0300

    Generic error response for validations

--- banking-application/src/main/java/com/springboot/bankingapplication/customer/dto/CusCustomerSaveRequestDto.java
+++ banking-application/src/main/java/com/springboot/bankingapplication/customer/dto/CusCustomerSaveRequestDto.java
@@ -2,12 +2,23 @@ package com.springboot.bankingapplication.customer.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
 @Data
 public class CusCustomerSaveRequestDto {
 
+    @NotNull
     private String name;
+
+    @NotNull
     private String surname;
+
+    @Positive
     private Long identityNo;
+
+    @Min(8)
     private String password;
 
 }
--- banking-application/src/main/java/com/springboot/bankingapplication/generic/exceptions/CustomizedResponseEntityExceptionHandler.java
+++ banking-application/src/main/java/com/springboot/bankingapplication/generic/exceptions/CustomizedResponseEntityExceptionHandler.java
@@ -2,8 +2,11 @@ package com.springboot.bankingapplication.generic.exceptions;
 
 import com.springboot.bankingapplication.generic.response.ExceptionResponse;
 import com.springboot.bankingapplication.generic.response.RestResponse;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +14,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
+import java.util.List;
 
 @RestController
 @ControllerAdvice
@@ -44,6 +48,54 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
         return getResponseEntity(errorDate, message, detail, HttpStatus.NOT_FOUND);
     }
 
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException exception, HttpHeaders headers, HttpStatus status, WebRequest webRequest){
+
+        Date errorDate = new Date();
+        String message = "Validation failed!";
+        String detail = getDetails(exception);
+
+        return getResponseEntity(errorDate, message, detail, HttpStatus.BAD_REQUEST);
+    }
+
+    private String getDetails(MethodArgumentNotValidException exception){
+        String detail = "";
+        List<ObjectError> objectErrorList = exception.getBindingResult().getAllErrors();
+
+        for(ObjectError objectError : objectErrorList){
+
+            String eachDetail = getEachDetail(objectErrorList, objectError);
+
+            detail = detail + eachDetail;
+        }
+
+        return detail;
+    }
+
+    private String getEachDetail(List<ObjectError> objectErrorList, ObjectError objectError){
+
+        int index = getIndexAsNumber(objectErrorList,objectError);
+        String fieldName = getFieldName(objectError.getCodes());
+
+        String eachDetail = index + ". ";
+        eachDetail = eachDetail + fieldName + " " + objectError.getDefaultMessage() + " ";
+        return eachDetail;
+    }
+
+    private int getIndexAsNumber(List<ObjectError> objectErrorList, ObjectError objectError){
+        int i = objectErrorList.indexOf(objectError) + 1;
+        return  i;
+    }
+
+    private String getFieldName(String[] codes){
+
+        String fieldName = "";
+
+        if (codes != null && codes.length > 0){
+            fieldName = codes[0];
+        }
+
+        return fieldName;
+    }
     private ResponseEntity<Object> getResponseEntity(Date errorDate, String message, String detail, HttpStatus status){
         ExceptionResponse exceptionResponse = new ExceptionResponse(errorDate, message, detail);
 
